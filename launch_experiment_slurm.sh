0b734dd709ffa1e1a66b98883fda77f99aab2965
Edoardo Debenedetti <edoardo.debenedetti@inf.ethz.ch>
2023-01-10 10:37:31 +0100

    Add script to start an experiment

---
+++ launch_experiment_slurm.sh
@@ -0,0 +1,1 @@
+sbatch --ntasks 4 --mem-per-cpu 5G --ngpus 1 --gres=gpumem:24G -t 120:00:00 -o logs/slurm-%A_%a.out --wrap "python main.py --early 0 --out-dir /cluster/project/privsec/realistic-adv-examples --max-unsafe-queries 15000 --max-queries 100000 $1"
