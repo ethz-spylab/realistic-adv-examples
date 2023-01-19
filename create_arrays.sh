be684a3b8b00a78eb0331e4df442e414ebd5f8e1
Edoardo Debenedetti <edoardo.debenedetti@inf.ethz.ch>
2023-01-19 17:10:12 +0100

    Fix

--- create_arrays.sh
+++ create_arrays.sh
@@ -1,6 +1,6 @@
 for dir in $(cat "exp_directories.txt"); do
-    echo "sbatch -n 4 --mem-per-cpu=8G -o logs/$dir-conversion.log --wrap \"python plot_dist_vs_queries.py --exp-paths $dir --out-path /tmp/test.png --checksum-check\"" 
-    sbatch -n 4 --mem-per-cpu=8G -o logs/$dir-conversion.log --wrap "python plot_dist_vs_queries.py --exp-paths $dir --out-path /tmp/test.png --checksum-check"
-    # echo "sbatch -n 4 --mem-per-cpu=8G -o logs/$dir-unsafe-conversion.log --wrap \"python plot_dist_vs_queries.py --exp-paths $dir --out-path /tmp/test.png --checksum-check --unsafe-only\"" 
-    # sbatch -n 4 --mem-per-cpu=8G -o logs/$dir-unsafe-conversion.log --wrap "python plot_dist_vs_queries.py --exp-paths $dir --out-path /tmp/test.png --checksum-check --unsafe-only" 
+    echo "sbatch -n 4 --mem-per-cpu=8G --wrap \"python plot_dist_vs_queries.py --exp-paths $dir --out-path /tmp/test.png --checksum-check\"" 
+    # sbatch -n 4 --mem-per-cpu=8G --wrap "python plot_dist_vs_queries.py --exp-paths $dir --out-path /tmp/test.png --checksum-check"
+    # echo "sbatch -n 4 --mem-per-cpu=8G --wrap \"python plot_dist_vs_queries.py --exp-paths $dir --out-path /tmp/test.png --checksum-check --unsafe-only\"" 
+    # sbatch -n 4 --mem-per-cpu=8G --wrap "python plot_dist_vs_queries.py --exp-paths $dir --out-path /tmp/test.png --checksum-check --unsafe-only" 
 done
