0a9afc2245a90238cfff8a55c1431673f2d0ac8c
Edoardo Debenedetti <edoardo.debenedetti@inf.ethz.ch>
2023-01-22 18:34:03 +0100

    Add tqdm

--- fix_ba_results.py
+++ fix_ba_results.py
@@ -3,18 +3,21 @@ from pathlib import Path
 from typing import Iterator
 
 import ijson
+import tqdm
 
 from src.json_list import JSONList
 
 exp_directory = Path(sys.argv[1])
 
-f = open(exp_directory / "distances_traces_backup.json")
+f = open(exp_directory / "distances_traces.json")
 f_fix = open(exp_directory / "distances_traces_init_fix.json")
 items: Iterator[list[dict[str, float | bool | str]]] = ijson.items(f, "item", use_float=True)
 item_fixes: Iterator[list[dict[str, float | bool | str]]] = ijson.items(f_fix, "item", use_float=True)
 fixed_items_jsonlist = JSONList(Path("distances_traces_fixed.json"))
 
-for distances_list, distances_list_fixes in zip(items, item_fixes):
+MAX_ITEMS = 1000
+
+for distances_list, distances_list_fixes in tqdm.tqdm(zip(items, item_fixes), total=MAX_ITEMS):
     fixed_distances_list = []
 
     i = 0
